df6480ab3a9f220defe7633e56eecb87cc83c235
viniciuslazzari <vinilazzari028@gmail.com>
2021-04-24 23:04:58 -0300

    ✨ Add sigmoid function

--- main.py
+++ main.py
@@ -20,6 +20,11 @@ def normalizeData(data):
 
 	return data
 
+def sigmoid(x, theta):
+	sigma = 1 / (1 + np.exp(-(np.sum(x_train * theta, 1))))
+
+	return sigma
+
 data = pd.read_csv('./data.csv')
 
 data = filterData(data)
@@ -32,4 +37,12 @@ theta = np.zeros(len(x.columns))
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0)
 
-print(x_test)
+x_train = np.array(x_train)
+y_train = np.array(y_train)
+x_test = np.array(x_test)
+y_test = np.array(y_test)
+theta = np.array(theta).T
+
+sigma = sigmoid(x_train, theta)
+
+print(sigma)
